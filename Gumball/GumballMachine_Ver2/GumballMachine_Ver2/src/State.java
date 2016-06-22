29b127aad735ebe5194c36a65c0bb046c6b93c0f
DeekshaZ <dsurasan@student.fitchburgstate.edu>
2016-06-21 23:04:03 -0400

    Initial commit

---
+++ Gumball/GumballMachine_Ver2/GumballMachine_Ver2/src/State.java
@@ -0,0 +1,12 @@
+
+public interface State {
+
+	public abstract void insertQuarter();
+
+	public abstract void ejectQuarter();
+
+	public abstract void turnCrank();
+
+	public abstract void dispense();
+
+}
